af48c3531a5931c61e096d531efd1278c53bf803
Unknown <marin.boone@edu.ece.fr>
2018-05-24 12:53:59 +0200

    Premier Push

---
+++ src/tp4/Portefeuille.java
@@ -0,0 +1,38 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp4;
+
+import java.util.Map;
+
+/**
+ *
+ * @author peti_
+ */
+public class Portefeuille {
+    //Attributs
+    //1 HashMap pour les fonds. Clé recherche String + Obj Fond
+    Map<String, Fonds> map_fonds;
+    //1 HashMap pour les instruments. Clé recherche String + Obj Instr
+    Map<String, Instrument> map_instru;
+    
+    //Constructeurs
+    public Portefeuille(){
+        
+    }
+    
+    //Methodes
+    public double recherche_fond(){
+        
+        return 102;
+    }
+    
+}
+
+
+
+
+
+
